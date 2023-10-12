71587e1441264946d238669db077179834458d97
C. Clauson <coclauso@microsoft.com>
2023-10-11 23:12:50 -0700

    Visual boxes on chart

--- src/App.tsx
+++ src/App.tsx
@@ -16,14 +16,9 @@ import { Line } from 'react-chartjs-2';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 // date-fns
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-// or for dayjs
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-// or for luxon
-import { AdapterLuxon } from '@mui/x-date-pickers/AdapterLuxon';
-// or for moment
-import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
-import { DatePicker } from '@mui/x-date-pickers';
 import { DateRange, DateRangePicker } from '@mui/x-date-pickers-pro';
+import Annotation, { AnnotationOptions } from 'chartjs-plugin-annotation';
+import annotationPlugin from 'chartjs-plugin-annotation';
 
 ChartJS.register(
   CategoryScale,
@@ -32,8 +27,32 @@ ChartJS.register(
   LineElement,
   Title,
   Tooltip,
-  Legend
+  Legend,
+  Annotation,
+  annotationPlugin
 );
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+const annotation1: AnnotationOptions = {
+  type: 'box',
+  backgroundColor: 'rgba(0, 150, 0, 0.04)',
+  borderColor: 'rgba(0, 150, 0, 0.4)',
+  borderRadius: 4,
+  borderWidth: 1,
+  xMax: 1.5,
+  xMin: 0.5
+};
+
+const annotation2: AnnotationOptions = {
+  type: 'box',
+  backgroundColor: 'rgba(150, 0, 0, 0.04)',
+  borderColor: 'rgba(150, 0, 0, 0.4)',
+  borderRadius: 4,
+  borderWidth: 1,
+  xMax: 3.5,
+  xMin: 2.5
+};
 
 export const options = {
   responsive: true,
@@ -41,27 +60,27 @@ export const options = {
     legend: {
       position: 'top' as const,
     },
-    title: {
-      display: true,
-      text: 'Chart.js Line Chart',
-    },
+    annotation: {
+      annotations: {
+        annotation1,
+        annotation2
+      }
+    }
   },
 };
-
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
 
 export const data = {
   labels,
   datasets: [
     {
       label: 'Dataset 1',
-      data: [1, 2, 3, 4, 5, 6, 7],
+      data: [1, 2, 3, 4, 5, 6],
       borderColor: 'rgb(255, 99, 132)',
       backgroundColor: 'rgba(255, 99, 132, 0.5)',
     },
     {
       label: 'Dataset 2',
-      data: [1, 3, 5, 7, 2, 4, 6],
+      data: [1, 3, 5, 2, 4, 6],
       borderColor: 'rgb(53, 162, 235)',
       backgroundColor: 'rgba(53, 162, 235, 0.5)',
     },
@@ -81,7 +100,7 @@ function App() {
         // - Volume Numbers are in individual shares
         // - Timestamps run from the start of the period (eg 1min bars stamped 09:30 run from 09:30.00 to 09:30.59)
         // - Times with zero volume are omitted (thus gaps in the data sequence are when there have been no trades)
-        console.log(line);
+        // console.log(line);
       }
     })();
   });
